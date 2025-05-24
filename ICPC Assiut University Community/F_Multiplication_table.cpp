dad0eb6c1e5439b158c51802f2c041cabbaa4442
Md Rakibuzzaman <rakibuzzaman740@gmail.com>
2025-05-24 09:13:55 +0600

    Sheet: 02 Problem: F. Multiplication table

---
+++ ICPC Assiut University Community/F_Multiplication_table.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+    fast;
+    int n; cin >> n;
+
+    for(int i = 1; i <= 12; i++)
+    {
+        int a = n * i;
+        cout << n << " * " << i << " = " << a << '\n';
+    }
+    return 0;
+}
