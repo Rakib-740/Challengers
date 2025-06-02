7ae627599de8fae1a4dbb278bf46c709a7327b08
Md Rakibuzzaman <rakibuzzaman740@gmail.com>
2025-06-02 08:52:06 +0600

    Add files via upload

---
+++ ICPC Assiut University Community/Sheet #3/F_Reversing.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+    fast;
+    int n; cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for(int i = n-1; i >= 0; i--)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
