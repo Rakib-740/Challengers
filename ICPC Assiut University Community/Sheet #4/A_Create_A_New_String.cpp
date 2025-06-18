304d19bd48d8643057c1ffd0394ca87bba8e46d6
Md Rakibuzzaman <rakibuzzaman740@gmail.com>
2025-06-18 14:24:41 +0600

    Sheet #4

---
+++ ICPC Assiut University Community/Sheet #4/A_Create_A_New_String.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+    fast;
+    string s, t; 
+    getline(cin, s);
+    getline(cin, t);
+    cout << s.length() << " " << t.length() << '\n' << s << " " << t << '\n';
+    return 0;
+}
