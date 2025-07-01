926b20fbfb6f6f7442b843533dd1146ed5a48df3
Md Rakibuzzaman <rakibuzzaman740@gmail.com>
2025-07-01 20:23:21 +0600

    B_Print

---
+++ ICPC Assiut University Community/Sheet #5/B_Print.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endll '\n'
+
+void print_to_n(int n)
+{
+    for(int i = 1; i <= n; i++)
+    {
+        cout << i ;
+        if(i < n)
+        {
+            cout << " ";
+        }
+    }
+    cout << endll;
+}
+
+int main() {
+    fast;
+    int n; cin >> n;
+
+    print_to_n(n);
+    return 0;
+}
