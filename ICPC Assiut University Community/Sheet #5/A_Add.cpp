73675ade5b1383e6fc2a040f296585fdfdaac358
Md Rakibuzzaman <rakibuzzaman740@gmail.com>
2025-07-01 20:18:10 +0600

    A_Add

---
+++ ICPC Assiut University Community/Sheet #5/A_Add.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endll '\n'
+
+int sum(int x, int y)
+{
+    return x + y;
+}
+
+int main() {
+    fast;
+    int x, y; cin >> x >> y;
+
+    cout << sum(x,y) << endll;
+    
+    return 0;
+}
