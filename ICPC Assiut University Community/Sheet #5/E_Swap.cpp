de9b262cf4276e62b1fce99f87fda32e67a842ec
Md Rakibuzzaman <rakibuzzaman740@gmail.com>
2025-07-02 21:37:30 +0600

    E_Swap

---
+++ ICPC Assiut University Community/Sheet #5/E_Swap.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endll '\n'
+
+void swap_two(int a, int b)
+{
+    cout << b << " " << a;
+}
+
+int main() {
+    fast;
+    int a, b; cin >> a >> b;
+    swap_two(a,b);
+    return 0;
+}
