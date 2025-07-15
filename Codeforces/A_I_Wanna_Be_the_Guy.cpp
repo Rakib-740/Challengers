90d9c4d8153bc932d3eaa5e722e2ab85cffc0932
Md Rakibuzzaman <rakibuzzaman740@gmail.com>
2025-07-15 21:38:57 +0600

    469A - I Wanna Be the Guy

---
+++ Codeforces/A_I_Wanna_Be_the_Guy.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endll '\n'
+
+int main() {
+    fast;
+    int n; cin >> n;
+
+    int x; cin >> x;
+
+    vector<int> a(x);
+
+    for(int i = 0; i < x; i++)
+    {
+        cin >> a[i];
+    }
+
+    int y; cin >> y;
+
+    vector<int> b(y);
+
+    for(int i = 0; i < y; i++)
+    {
+        cin >> b[i];
+    }
+
+    set<int> arr;
+
+    for(int i = 0; i < x; i++)
+    {
+        arr.insert(a[i]);
+    }
+
+    for(int i = 0; i < y; i++)
+    {
+        arr.insert(b[i]);
+    }
+    
+    if(n == arr.size())
+    {
+        cout << "I become the guy." << endll;
+    }
+    else
+    {
+        cout << "Oh, my keyboard!" << endll;
+    }
+    return 0;
+}
