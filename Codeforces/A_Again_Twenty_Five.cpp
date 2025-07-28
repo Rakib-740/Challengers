97f687eae46f7db6ee94f64e43232fb8dd4965b9
Md Rakibuzzaman <rakibuzzaman740@gmail.com>
2025-07-28 21:43:07 +0600

    A_Again_Twenty_Five.cpp

---
+++ Codeforces/A_Again_Twenty_Five.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define endll '\n'
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s; cin >> s;
+
+    cout << "25" << endll;
+    return 0;
+}
