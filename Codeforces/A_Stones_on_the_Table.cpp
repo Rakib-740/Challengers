195e9998b76865cca1ad04c301af1816b57180ad
Md Rakibuzzaman <rakibuzzaman740@gmail.com>
2025-08-03 21:28:54 +0600

    266A - Stones on the Table

---
+++ Codeforces/A_Stones_on_the_Table.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define endll '\n'
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n; cin >> n;
+
+    string s; cin >> s;
+    int count = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(s[i] == s[i + 1])
+        {
+            count++;
+        }
+    }
+    cout << count << endll;
+    return 0;
+}
