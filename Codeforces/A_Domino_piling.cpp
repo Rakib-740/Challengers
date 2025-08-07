7f15f2e68919aaee491864b6d2ee8034db8b3f8f
Md Rakibuzzaman <rakibuzzaman740@gmail.com>
2025-08-07 22:38:30 +0600

    50A - Domino piling

---
+++ Codeforces/A_Domino_piling.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define endll '\n'
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int m, n;
+    cin >> m >> n;
+
+    // int domino = (m * n) / 2;
+
+    cout << (m * n) / 2 << endll;
+    return 0;
+}
