00130bd45c6ea14d1b51bd035986454399b85b9b
Md Rakibuzzaman <rakibuzzaman740@gmail.com>
2025-08-19 23:36:15 +0600

    1385A - Three Pairwise Maximums

---
+++ Codeforces/A_Three_Pairwise_Maximums.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endll '\n'
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int x, y, z; 
+        cin >> x >> y >> z;
+
+        int mx = max({x, y, z});
+        int cnt = (x==mx) + (y==mx) + (z==mx);
+
+        if (cnt < 2) {
+            cout << "NO" << endll;
+            continue;
+        }
+
+        cout << "YES" << endll;
+        int s;
+        if (x==mx && y==mx) {            // x,y are the two maxima
+            s = z;
+            cout << mx << " " << s << " " << s << endll;
+        } else if (x==mx && z==mx) {     // x,z are the two maxima
+            s = y;
+            cout << s << " " << mx << " " << s << endll;
+        } else {                         // y,z are the two maxima
+            s = x;
+            cout << s << " " << s << " " << mx << endll;
+        }
+    }
+    return 0;
+}
