c6a76264e0bc39ec93464251a9890efd39e7dcb0
Md Rakibuzzaman <rakibuzzaman740@gmail.com>
2025-08-31 22:22:00 +0600

    2094A - Trippi Troppi

---
+++ Codeforces/A_Trippi_Troppi.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define endll '\n'
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t; cin >> t;
+
+    while(t--)
+    {
+        string s, b, c;
+        cin >> s >> b >> c;
+        
+
+        cout << s[0] << b[0] << c[0] << endll;
+    }
+    return 0;
+}
