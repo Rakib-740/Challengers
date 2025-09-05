a97831ad4ef007ac6470cb12b85cae8a8a47591e
Md Rakibuzzaman <rakibuzzaman740@gmail.com>
2025-09-05 20:44:58 +0600

    Add files via upload

---
+++ Codeforces/B_Maximum_Multiple_Sum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define endll '\n'
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t; cin >> t;
+
+    while(t--)
+    {
+        int n; cin >> n;
+
+        if(n == 3)
+        {
+            cout << "3" << endll;
+        }
+        else
+        {
+            cout << "2" << endll;
+        }
+    }
+    return 0;
+}
